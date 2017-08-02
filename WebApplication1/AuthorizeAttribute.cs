108bafcac661d8b0b4020017b7792a5e6cd7d6b4
Chuong Bui <chuong.bui@ntq-solution.com.vn>
2017-08-02 11:53:49 +0700

    first

---
+++ WebApplication1/AuthorizeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web;
+
+
+namespace WebApplication1
+{
+    public class AuthorizeAttribute : System.Web.Http.AuthorizeAttribute
+    {
+        protected override void HandleUnauthorizedRequest(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            if (!HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                base.HandleUnauthorizedRequest(actionContext);
+            }
+            else
+            {
+                actionContext.Response = new System.Net.Http.HttpResponseMessage(System.Net.HttpStatusCode.Forbidden);
+            }
+        }
+    }
+}
---
+++ WebApplication1/Controllers/DataController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class DataController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/data/forall")]
+        public IHttpActionResult Get()
+        {
+            return Ok("Server time : " + DateTime.Now.ToString());
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("api/data/authenticate")]
+        public IHttpActionResult GetForAuth()
+        {
+            return Ok("You are authed\n" + User.Identity.Name);
+        }
+
+        [Authorize(Roles ="admin")]
+        [HttpGet, HttpOptions]
+        [Route("api/data/authorize")]
+        public IHttpActionResult GetForAdmin()
+        {
+            return Ok("You are admin\n" + User.Identity.Name);
+        }
+
+    }
+}
